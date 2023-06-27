ea489cac4c6fded40f885d0faac9093ce3974075
FuwaFuwa0204 <soyeonmail24@gmail.com>
2023-06-27 17:31:21 +0900

    add RadioButton,Switch

---
+++ app/src/main/java/com/example/tiptime_view/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tiptime_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
